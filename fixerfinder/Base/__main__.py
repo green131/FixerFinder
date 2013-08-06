6abdc500c26acf7705834ba979e1c2fb433619b9
Newser <singkiwi@hotmail.com>
2013-08-06 16:56:10 -0400

    Updated findOriginal
    - Now uses date and time to compare relevancy
    - Untested method of comparing fixed post with search_result
    - FIXED added keyword parameter to isFixed function

--- fixerfinder/Base/__main__.py
+++ fixerfinder/Base/__main__.py
@@ -1,11 +1,12 @@
 import time
+import datetime
 import praw
 
 global done
 global fixed
 global keywords
 
-def isFixed(title):
+def isFixed(title, keywords):
     return any(string in title for string in keywords)
 
 def findFixed():
@@ -20,7 +21,7 @@ def findFixed():
         if i%100 == 0:
             print(str(i/10) + "%")
         title_lwr = submission.title.lower()
-        if submission.id not in done and isFixed(title_lwr):
+        if submission.id not in done and isFixed(title_lwr, keywords):
             fixed.append(submission.id)
             print("isFixed: (" + submission.id + ") " + title_lwr)
             findOriginal(title_lwr)
@@ -36,20 +37,35 @@ def findOriginal(title):
             break
     x=0
     for search_result in r.search(title):
-        fixed = 0
-        if isFixed(search_result):
-            fixed = 1
-        print("Search Result " + str(x+1) + ":" + str(search_result) + "\n"
+        #Make search_result title lowercase
+        search_result.title = search_result.title.lower()
+        #Find date created
+        created_date = datetime.date(search_result.created)
+        created_time = datetime.time(search_result.created)
+        #Identify if search_result isFixed
+        fixed = isFixed(search_result.title, keywords)
+        #DEBUG
+        print("Search Result " + str(x+1) + ": " + str(search_result.title) + "\n"
               + "-- ID: " + str(search_result.id) + "\n"
-              + "-- Created: " + str(search_result.created) + "\n"
+              + "-- Created (day): " + str(created_date) + "\n"
+              + "-- Created (time): " + str(created_time) + "\n"
               + "-- Contains Fixed: " + str(fixed))
+        #Compare search_result with fixed
+        if (created_date.day == datetime.datetime.now().date().day):
+            print("Same day match...")
+            if (search_result.title == title):
+                print("MATCH FOUND: \n" 
+                    + search_result.title + "\n"
+                    + title)
+        #Counter
         x+=1
-        if x>9:
+        if x>(max_searching-1):
             break
 
 
 #-------------------Main Script-------------------------
 sleep=60
+max_searching=15
 if __name__ == "__main__":
     #Login & Connection
     r = praw.Reddit(user_agent="fixerfinder")
